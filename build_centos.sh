a3b90bc586cad2206782be7bf389b04f7bc80ef6
tomzombie <21fcfd72@opayq.com>
2016-07-23 12:31:35 -0700

    Update build_centos.sh
    changing order of operations.

--- build_centos.sh
+++ build_centos.sh
@@ -7,17 +7,7 @@ adduser tomzombie
 passwd tomzombie
 gpasswd -a tomzombie wheel
 
-sed -i 's/BOOTPROTO\=\"dhcp\"/BOOTPROTO\=\"static\"/' /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "IPADDR=192.168.1.6" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "NETMASK=255.255.255.0" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "GATEWAY=192.168.1.1" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "DNS1=192.168.1.1" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-#echo "HOSTNAME=pignuckle" >> /etc/sysconfig/network
-#echo "DNS1=192.168.1.1" >> /etc/sysconfig/network
-systemctl restart network
-
 cat <<HAIL_ERIS> /etc/issue.net
-
 ###################################################################################
 # Authorized access only!                                                         #
 # Disconnect IMMEDIATELY if you are not an authorized user!!!                     #
@@ -28,3 +18,10 @@ HAIL_ERIS
 sed -i 's/\#Banner none/Banner \/etc\/issue\.net/' /etc/ssh/sshd_config
 sed -i 's/\#PermitRootLogin\ yes/PermitRootLogin\ no/' /etc/ssh/sshd_config
 systemctl restart sshd
+
+sed -i 's/BOOTPROTO\=\"dhcp\"/BOOTPROTO\=\"static\"/' /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "IPADDR=192.168.1.6" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "NETMASK=255.255.255.0" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "GATEWAY=192.168.1.1" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "DNS1=192.168.1.1" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+systemctl restart network
