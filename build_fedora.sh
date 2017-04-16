4ddc185687afe5766d345baa78e111e3f8869890
tomzombie <21fcfd72@opayq.com>
2017-04-16 12:19:47 -0700

    adding fedora build

---
+++ build_fedora.sh
@@ -0,0 +1,32 @@
+sudo su -
+timedatectl set-timezone America/Phoenix
+hostnamectl set-hostname demonica
+yum -y update
+yum -y install nano
+yum -y install pam_yubico
+
+cat <<HAIL_ERIS> /etc/issue.net
+###################################################################################
+# Authorized access only!                                                         #
+# Disconnect IMMEDIATELY if you are not an authorized user!!!                     #
+# All actions Will be monitored and recorded                                      #
+###################################################################################
+HAIL_ERIS
+
+sed -i 's/\#Banner none/Banner \/etc\/issue\.net/' /etc/ssh/sshd_config
+sed -i 's/\#PermitRootLogin\ yes/PermitRootLogin\ no/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+mkdir -p /root/git/ddate
+git clone https://github.com/tomzombie/ddate.git /root/git/ddate
+yum install -y cmake
+yum install -y gcc
+cmake /root/git/ddate/
+make /root/git/ddate/
+make install /root/git/ddate
+
+systemctl stop httpd.service
+systemctl disable httpd.service
+systemctl stop bluetooth.service
+systemctl disable bluetooth.service
+
