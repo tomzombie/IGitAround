85ceb82dfd0ca70b5cf24ee4f1ea12d7d0194d2e
tomzombie <21fcfd72@opayq.com>
2024-12-01 14:09:30 -0500

    Update build_fedora.sh
    updating to dnf and removing gnome display manager

--- build_fedora.sh
+++ build_fedora.sh
@@ -1,16 +1,16 @@
 sudo su -
-timedatectl set-timezone America/Phoenix
-hostnamectl set-hostname demonica
-yum -y update
-yum -y install nano
-yum -y install pam_yubico
+timedatectl set-timezone America/New York
+hostnamectl set-hostname diabolical
+dnf -y update
+dnf -y install nano
+dnf -y install pam_yubico
 
 cat <<HAIL_ERIS> /etc/issue.net
-###################################################################################
-# Authorized access only!                                                         #
-# Disconnect IMMEDIATELY if you are not an authorized user!!!                     #
-# All actions Will be monitored and recorded                                      #
-###################################################################################
+################################################################
+# Authorized access only!                                      #
+# Disconnect IMMEDIATELY if you are not an authorized user!!!  #
+# All actions Will be monitored and recorded                   #
+################################################################
 HAIL_ERIS
 
 sed -i 's/\#Banner none/Banner \/etc\/issue\.net/' /etc/ssh/sshd_config
@@ -19,8 +19,8 @@ systemctl restart sshd
 
 mkdir -p /root/git/ddate
 git clone https://github.com/tomzombie/ddate.git /root/git/ddate
-yum install -y cmake
-yum install -y gcc
+dnf install -y cmake
+dnf install -y gcc
 cmake /root/git/ddate/
 make /root/git/ddate/
 make install /root/git/ddate
@@ -29,6 +29,8 @@ systemctl stop httpd.service
 systemctl disable httpd.service
 systemctl stop bluetooth.service
 systemctl disable bluetooth.service
+systemctl stop gdm
+systemctl disable gdm
 
 #yubi key set up  stuff
 auth sufficient pam_yubico.so debug id=1 authfile=/etc/yubikeys
