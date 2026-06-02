188c455db61ba4d4ef1dc6cbf4e8281488ede471
tomzombie <21fcfd72@opayq.com>
2026-06-02 11:17:41 -0400

    Update build_fedora.sh
    update after the old raspberry pi died and you upgraded to the brick

--- build_fedora.sh
+++ build_fedora.sh
@@ -18,6 +18,7 @@ systemctl restart sshd
 
 #disable selinux
 sed -i 's/SELINUX\=enforcing/SELINUX\=disabled/' /etc/selinux/config
+grubby --update-kernel ALL --args selinux=0
 
 mkdir -p /root/git/ddate
 git clone https://github.com/tomzombie/ddate.git /root/git/ddate
@@ -33,8 +34,22 @@ systemctl disable gdm
 
 dnf remove -y cups
 
+#set static ip address
+nmcli connection modify "Wired connection 1" ipv4.addresses 192.168.1.23/24
+nmcli connection modify "Wired connection 1" ipv4.gateway 192.168.1.1
+nmcli connection modify "Wired connection 1" ipv4.dns "8.8.8.8 1.1.1.1"
+nmcli connection modify "Wired connection 1" ipv4.method manual
+nmcli connection down "Wired connection 1" && nmcli connection up "Wired connection 1"
+
+#set firewall rules for dns and http
+firewall-cmd --add-port=53/tcp --permanent
+firewall-cmd --add-port=53/udp --permanent
+firewall-cmd --add-port=80/tcp --permanent
+firewall-cmd --reload
+
+#you must reboot before you set up pihole. selinux can't be fully turned off without reboot and you need to make sure that the ip address is static
 curl -sSL https://install.pi-hole.net | bash
 
-#yubi key set up  stuff
+#yubi key set up  stuff but you don't use it
 auth sufficient pam_yubico.so debug id=1 authfile=/etc/yubikeys
 reboot
